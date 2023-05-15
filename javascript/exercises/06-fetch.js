1334cd053e29106fb7768d4dd70e37f6e9a67fcc
lumi-404 <130887758+lumi-404@users.noreply.github.com>
2023-05-14 23:13:26 -0700

    Completed JavaScript Exercises 1-7

--- javascript/exercises/06-fetch.js
+++ javascript/exercises/06-fetch.js
@@ -1,9 +1,56 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
+app.style.paddingLeft = 0;
+const loading = document.querySelector("#loading");
+
+const addBookToDOM = (book) => {
+  console.log(book);
+  let element = document.createElement("div");
+  let title = document.createElement("h4");
+  let author = document.createElement("p");
+  let published = document.createElement("p");
+  let pages = document.createElement("p");
+
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
+
+  title.textContent = book.name;
+  author.textContent = `by ${book.authors[0]}`;
+  published.textContent = book.released.substr(0, 4);
+  pages.textContent = `${book.numberOfPages} pages`;
+
+  element.append(title);
+  element.append(author);
+  element.append(published);
+  element.append(pages);
+
+  app.append(element);
+};
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item) => {
+        addBookToDOM(item);
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      let div = document.createElement("div");
+      div.textContent = `An error occurred. Please reload the page.`;
+      app.append(div);
+    })
+    .finally(() => {
+      app.removeChild(loading);
+    });
 };
+
+fetchData(url);
